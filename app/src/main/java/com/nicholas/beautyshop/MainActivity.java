def22fe2fa9658c03ce8cd98c1d17c1d0888f2a5
Nicholas Nzovia <nichonzovia@gmail.com>
2021-08-08 20:26:14 -0400

    products category, diplays the category that the shopper deals with. While the product is the individual item

--- app/src/main/java/com/nicholas/beautyshop/MainActivity.java
+++ app/src/main/java/com/nicholas/beautyshop/MainActivity.java
@@ -6,15 +6,18 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.nicholas.beautyshop.adapters.ProductAdapter;
 import com.nicholas.beautyshop.adapters.ProductCategoryAdapter;
 import com.nicholas.beautyshop.appmodel.ProductCategory;
+import com.nicholas.beautyshop.appmodel.ProductsModel;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     ProductCategoryAdapter productCategoryAdapter;
-    RecyclerView productRecyclerView;
+    RecyclerView productRecyclerView, productListRecyclerview;
+    ProductAdapter productAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,9 +29,14 @@ public class MainActivity extends AppCompatActivity {
         productCategoryList.add(new ProductCategory(1,"Most Popular"));
         productCategoryList.add(new ProductCategory(2,"Skin Care"));
         productCategoryList.add(new ProductCategory(3,"Hair"));
-        productCategoryList.add(new ProductCategory(4,"Nail"));
+        productCategoryList.add(new ProductCategory(4,"Nail Care"));
 
         setProductRecyclerView(productCategoryList);
+
+        //add data from the holder to the model
+        List<ProductsModel>  productsModelList = new ArrayList<>();
+        productsModelList.add(new ProductsModel());
+
 
     }
     //setting up a recyclerView in an horizontal format
@@ -37,6 +45,14 @@ public class MainActivity extends AppCompatActivity {
         RecyclerView.LayoutManager layoutManager=new LinearLayoutManager(this,RecyclerView.HORIZONTAL,false);
         productRecyclerView.setLayoutManager(layoutManager);
         productCategoryAdapter = new ProductCategoryAdapter(this,productCategoryList);
+        productRecyclerView.setAdapter(productCategoryAdapter);
+    }
+    //setting up a recyclerView in an horizontal format
+    private void setProductListRecyclerview(List<ProductsModel> beautyProducts){
+        productListRecyclerview=findViewById(R.id.productListRecycler);
+        RecyclerView.LayoutManager layoutManager=new LinearLayoutManager(this,RecyclerView.HORIZONTAL,false);
+        productListRecyclerview.setLayoutManager(layoutManager);
+        productAdapter = new ProductAdapter(this,beautyProducts);
         productRecyclerView.setAdapter(productCategoryAdapter);
     }
 }
--- app/src/main/java/com/nicholas/beautyshop/adapters/ProductAdapter.java
+++ app/src/main/java/com/nicholas/beautyshop/adapters/ProductAdapter.java
@@ -1,46 +1,66 @@
 package com.nicholas.beautyshop.adapters;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.nicholas.beautyshop.R;
 import com.nicholas.beautyshop.appmodel.ProductsModel;
 
 import java.util.List;
 
 public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ProductViewHolder> {
     Context context;
-    List<ProductsModel> beautyProducts;
+    List<ProductsModel> productsModelList;
 
     //generate a constructor for the products
-    public ProductAdapter(List<ProductsModel> beautyProducts) {
-        this.beautyProducts = beautyProducts;
+    public ProductAdapter(Context context, List<ProductsModel> productsModelList) {
+        this.context = context;
+        this.productsModelList = productsModelList;
     }
 
     @NonNull
     @Override
     public ProductViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view= LayoutInflater.from(context).inflate(R.layout.productslist,parent,false);
+        return new ProductViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ProductViewHolder holder, int position) {
+        //pass the products to the holder
+        holder.productImage.setImageResource(productsModelList.get(position).getImageUrl());
+        holder.productName.setText(productsModelList.get(position).getProductname());
+        holder.productSize.setText(productsModelList.get(position).getProductquantity());
+        holder.productPrice.setText(productsModelList.get(position).getProductprice());
+
 
     }
 
     @Override
     public int getItemCount() {
-        return beautyProducts.size();
+        return productsModelList.size();
     }
 
     //create a viewholder class that extends to recyclerview.viewholder
     public static final class ProductViewHolder extends RecyclerView.ViewHolder{
-
+        //accessing the products item widgets
+        ImageView productImage;
+        TextView productName, productSize, productPrice;
         public ProductViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            productImage=itemView.findViewById(R.id.productimage);
+            productName=itemView.findViewById(R.id.productname);
+            productSize=itemView.findViewById(R.id.productsize);
+            productPrice=itemView.findViewById(R.id.productprice);
+
         }
     }
 }
